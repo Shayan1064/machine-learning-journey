8a9d721dcb6cdf878b5ec5cb9f1b110a7538be91
Shayan Hassan <gamingfanatics2115@gmail.com>
2026-08-01 01:29:00 +0500

    Day 1: Python Fundementals

---
+++ Day 1/Main.py
@@ -0,0 +1,85 @@
+# print("I am Shayan Hassan\nI am Cs Student\nI am learning Machine Learning")
+
+# name="Shayan"
+# age=10
+# character='A'
+# number=12.5
+# print("Name: ",name,"\nAge: ",age)
+# print(number)
+
+# Student Information
+
+# name = "Shayan"
+# age = 20
+# university = "UET Peshawar"
+# cgpa = 3.75
+# is_student = True
+
+# print("Student Information")
+# print("Name:", name)
+# print("Age:", age)
+# print("University:", university)
+# print("CGPA:", cgpa)
+# print("Is Student:", is_student)
+
+# Product Information
+
+# product_name = "Laptop"
+# price = 75000.50
+# quantity = 12
+# in_stock = True
+
+# print("Product Details")
+# print("Product:", product_name)
+# print("Price:", price)
+# print("Quantity:", quantity)
+# print("Available:", in_stock)
+
+# Data Types Practice
+
+# name = "Ali"
+# age = 22
+# height = 5.9
+# is_graduated = False
+
+# print(name, type(name))
+# print(age, type(age))
+# print(height, type(height))
+# print(is_graduated, type(is_graduated))
+
+# num1=10
+# num2=10
+# avg_number=(num1+num2)/2
+# print(avg_number)
+
+# name=(input("Enter Your Name: "))
+# print("Hello: ",name)
+
+# print(5=='5')
+# print(5 is '5')
+
+# a=5
+# b=5
+
+# print(a is b)
+# print(a==b)
+
+# name='Shayan'
+#  print(type(name))
+# name=int(name)
+# print(type(name))
+
+# number=22
+# number=float(number)
+# print(type(number))
+
+# print("Celsius to Fahreheit Converter")
+# cel=int(input("Enter Temprature in Celsius: "))
+# fah=(cel*(9/5))+32
+# print("Temprature in Fahreheit: ",fah)
+
+# number='45'
+# number1=int(number)
+# print(type(number1))
+# number2=float(number)
+# print(type(number2))
